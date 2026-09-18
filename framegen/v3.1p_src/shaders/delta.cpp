a4b6dd4a55d650b9842c8d45cc22f4f6b87ae5be
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-18 15:30:20 -0500

    feat(framegen): apply explicit interpolation timestamp

--- framegen/v3.1p_src/shaders/delta.cpp
+++ framegen/v3.1p_src/shaders/delta.cpp
@@ -3,6 +3,7 @@
 
 #include "v3_1p/shaders/delta.hpp"
 #include "common/utils.hpp"
+#include "pool/resourcepool.hpp"
 #include "core/commandbuffer.hpp"
 #include "core/image.hpp"
 
@@ -206,8 +207,11 @@ Delta::Delta(Vulkan& vk, std::array<std::array<Core::Image, 2>, 3> inImgs1,
 }
 
 void Delta::Dispatch(const Core::CommandBuffer& buf, uint64_t frameCount,
-        uint64_t pass_idx, size_t activeGenerationCount) {
+        uint64_t pass_idx, size_t activeGenerationCount,
+        float interpolationPhase) {
     auto& pass = this->passesByGenerationCount.at(activeGenerationCount).at(pass_idx);
+    if (interpolationPhase > 0.0F)
+        LSFG::Pool::ResourcePool::writeTimestamp(pass.buffer, interpolationPhase);
 
     // first shader
     const auto extent = this->tempImgs1.at(0).getExtent();
