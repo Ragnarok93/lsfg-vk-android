0f7e36abe066cfa857e82fa300d1c79b88d33f54
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-18 15:29:32 -0500

    feat(framegen): accept explicit interpolation phase

--- framegen/v3.1_include/v3_1/shaders/delta.hpp
+++ framegen/v3.1_include/v3_1/shaders/delta.hpp
@@ -46,7 +46,8 @@ namespace LSFG_3_1::Shaders {
         /// Dispatch the shaderchain.
         ///
         void Dispatch(const Core::CommandBuffer& buf, uint64_t frameCount,
-            uint64_t pass_idx, size_t activeGenerationCount);
+            uint64_t pass_idx, size_t activeGenerationCount,
+            float interpolationPhase = -1.0F);
 
         /// Get the first output image
         [[nodiscard]] const auto& getOutImage1() const { return this->outImg1; }
