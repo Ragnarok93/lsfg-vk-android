8c6f2d823c1274264e068139a39aea851eb9a40e
Ragnarok93 <e_fontenot1@yahoo.com>
2026-09-18 19:35:10 -0500

    feat(timing): track source timeline interval continuity

--- include/adaptive_scheduler.hpp
+++ include/adaptive_scheduler.hpp
@@ -52,6 +52,7 @@ private:
     bool initialized_{false};
     uint64_t sourceIndex_{0};
     uint64_t sourceDesiredTimeNs_{0};
+    uint64_t lastIntervalNs_{0};
 };
 
 /// Chooses the minimum number of interpolation frames needed to approach an
